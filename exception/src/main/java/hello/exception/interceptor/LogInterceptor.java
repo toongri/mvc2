c1e28bc4bbc20ff170cf74d96a29297b50976091
Toong_laptop <jhkim03284@gmail.com>
2022-04-03 23:00:06 +0900

    버그 수정

--- exception/src/main/java/hello/exception/interceptor/LogInterceptor.java
+++ exception/src/main/java/hello/exception/interceptor/LogInterceptor.java
@@ -11,7 +11,7 @@ import java.util.UUID;
 @Slf4j
 public class LogInterceptor implements HandlerInterceptor {
 
-    public static final String LOG_ID = "LogId";
+    public static final String LOG_ID = "logPw";
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
